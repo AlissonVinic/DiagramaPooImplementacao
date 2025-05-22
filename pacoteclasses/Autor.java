621fb04fd3546cc66f4864c4e3600a0b390f8d44
AlissonVinic <alissonvincns@gmail.com>
2025-05-22 15:20:31 -0300

    autor

--- pacoteclasses/Autor.java
+++ pacoteclasses/Autor.java
@@ -1,6 +1,53 @@
 package pacoteclasses;
 
-
 public class Autor {
-    
+
+    private String nome;
+    private String endereco;
+    private String cpf;
+    private Obra obra;
+
+    public String getNome()
+    {
+        return nome;
+    }
+    public String getEndereco()
+    {
+        return endereco;
+    }
+    public String getCpf()
+    {
+        return cpf;
+    }
+    public Obra getObra()
+    {
+        return obra;
+    }
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+    public void setEndereco(String endereco)
+    {
+        this.endereco = endereco;
+    }
+
+    public void setCpf(String cpf)
+    {
+        this.cpf = cpf;
+    }
+    public void setObra(Obra obra)
+    {
+        this.obra = obra;
+    }
+    public void verObra(Obra obra)
+    {
+        System.out.println("Título: " + obra.getTitulo());
+        System.out.println("autor: " + obra.getAutor());
+        System.out.println("Ano: " + obra.getAno());
+        System.out.println("Genero: " + obra.getGenero());
+        System.out.println("Status: " + obra.getStatus());
+        System.out.println("");
+    }
 }
+
