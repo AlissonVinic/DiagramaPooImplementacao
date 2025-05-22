f522cbaf071d991d7af978e4a2f55c613accc534
AlissonVinic <alissonvincns@gmail.com>
2025-05-22 14:36:13 -0300

    Classes obra e avaliador adicionadas

---
+++ classes/Avaliador.java
@@ -0,0 +1,49 @@
+package classes;
+
+
+public class Avaliador {
+
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private Obra obraParaAvaliar;
+
+    public String getNome()
+    {
+        return nome;
+    }
+    public String getCpf()
+    {
+        return cpf;
+    }
+    public String getEndereco()
+    {
+        return endereco;
+    }
+    public Obra getObraParaAvaliar()
+    {
+        return obraParaAvaliar;
+    }//
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+    public void setCpf(String cpf)
+    {
+        this.cpf = cpf;
+    }
+    public void setEndereco(String endereco)
+    {
+        this.endereco = endereco;
+    }
+    public void setObraParaAvaliar(Obra obraParaAvaliar)
+    {
+        this.obraParaAvaliar = obraParaAvaliar;
+    }
+    public void avaliarObra(String titulo, String comentario)
+    {
+        System.out.println("Avaliando obra: " + titulo + "\n Comentário: " + comentario);
+    }
+}
+
---
+++ classes/Obra.java
@@ -0,0 +1,58 @@
+package classes;
+
+public class Obra {
+    private String titulo;
+    private int ano;
+    private String status;
+    private Avaliador avaliador;
+    private Autor autor;
+    private String genero;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Avaliador getAvaliador() {
+        return avaliador;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setAvaliador(Avaliador avaliador) {
+        this.avaliador = avaliador;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
